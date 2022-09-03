1b26d28e4c70d1260b16e11bc41bc1c9d174c328
acorn13 <103873059+acorn13@users.noreply.github.com>
2022-09-02 22:02:11 -0500

    Delete idle-programmer directory

--- idle-programmer/js/components/Game.js
+++
@@ -1,7 +0,0 @@
-
-class Game extends Component {
-    render() {
-        this.path = 'ui.';
-        return `<div id="cmp-ui." class="cmp-updated">` + this.child(Clicker, {key: 'clicker'}) + this.child(Adder, {key: 'adder'}) + `<div>`;
-    }
-}
